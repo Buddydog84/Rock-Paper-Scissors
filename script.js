b45800cdb29821fe54f68ca2ee2a9a0dc5dc4181
buddydog84 <buddydog90@gmail.com>
2024-12-10 21:30:50 -0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("testing something out");
+function getComputerChoice() {
+    const randomNumber = Math.random();  // Generate a random number between 0 and 1
+    
+    if (randomNumber < 0.33) {
+      return "rock";
+    } else if (randomNumber < 0.66) {
+      return "paper";
+    } else {
+      return "scissors";
+    }
+  }
+
+  console.log(getComputerChoice());
